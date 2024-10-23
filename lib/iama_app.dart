1ede4e335c6a835a589ad16dcb7264a1c44e3c45
aquitan <aquitan@mail.ru>
2024-10-23 22:30:45 +0300

    add debugger - talker

--- lib/iama_app.dart
+++ lib/iama_app.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ia_ma/router/router.dart';
 import 'package:ia_ma/ui/theme/theme.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 class IamaApp extends StatefulWidget {
   const IamaApp({super.key});
@@ -19,7 +21,9 @@ class _IamaAppState extends State<IamaApp> {
       debugShowCheckedModeBanner: false,
       title: 'YaMa',
       theme: lightThemeData,
-      routerConfig: appRouter.config(),
+      routerConfig: appRouter.config(
+        navigatorObservers: () => [TalkerRouteObserver(GetIt.I<Talker>())],
+      ),
       // routes: {
       //   '/': (context) => const HomeScreen(),
       // },
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:ia_ma/iama_app.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 void main() {
+  final talker = TalkerFlutter.init();
+
+  GetIt.I.registerSingleton<Talker>(talker);
+  GetIt.I<Talker>().debug('Talker started...');
+
   runApp(const IamaApp());
 }
 
